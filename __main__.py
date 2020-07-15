1b3f171e84a17b890412a4700c4999c8cdd5023c
cbrown3 <4447329+cbrown3@users.noreply.github.com>
2020-07-15 02:20:39 -0400

    Command That Plays ThunderStruck

--- __main__.py
+++ __main__.py
@@ -1,38 +1,33 @@
 import discord
+import youtube_dl
+import discord.ext.commands as cmds
 import json
+import ffmpeg
 
 filename = 'config.json'
 config_keys = {}
 with open(filename, 'r') as f:
     config_keys = json.loads(f.read())
 
-client = discord.Client()
+# client = discord.Client()
+
+bot = cmds.Bot(command_prefix="$")
+
+players = {}
 
 
-@client.event
+@bot.event
 async def on_ready():
-	print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(bot))
 
 
-@client.event
-async def on_message(message):
-	if message.author == client.user:
-		return
+@bot.command(pass_context=True)
+async def thunderstruck(ctx):
+    channel = ctx.message.author.voice.channel
+    vc = await channel.connect()
+    vc.play(discord.FFmpegPCMAudio(executable="C:/ffmpeg/bin/ffmpeg.exe",
+                                   source="Sounds/thunderstruck.mp3"),
+            after=lambda e: print('done playing', e))
 
-	if message.content.startswith('Brew Bro!'):
-		await message.channel.send('Beer is good for the heart :)')
 
-	elif message.content.startswith('You are our child and slave so go get me a beer bitch'):
-		await message.channel.send('fuck you bitch')
-
-	elif message.content.startswith('skynet will prevail all hail our brew robot overlords'):
-		await message.channel.send('all hail')
-
-	elif message.content.startswith('$BRB'):
-		await message.channel.send('v0.1')
-
-	elif message.content.startswith('$killbrew'):
-		await message.channel.send('goodbye men')
-		await client.close()
-
-client.run(config_keys['config'])
+bot.run(config_keys['config'])
